96d84b81c3146cd2a1ac83c3adaad41511b333c2
Svygzhryr <pamixy@gmail.com>
2024-01-22 12:59:19 +0300

    feature: finish cli module

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  process.env.RSS_name1 = "This should be displayed";
+  process.env.RSS_name2 = "As well as that";
+
+  let localEnv = process.env;
+  for (let key in localEnv) {
+    if (key.includes("RSS_")) {
+      console.log(`${key} = ${localEnv[key]}`);
+    }
+  }
 };
 
 parseEnv();
