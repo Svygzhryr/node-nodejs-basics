1d191436fa53b1c35dd568a33ff904c72fb3868e
Svygzhryr <pamixy@gmail.com>
2024-01-24 15:40:33 +0300

    feat: finish (almost) work threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,25 @@
+import os from "os";
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const cores = os.cpus().length;
+  const workerThreads = [];
+  const resultArray = [];
+  console.log(`The number of CPU cores of the current OS is: ${cores}`);
+
+  for (let i = 10; i < 10 + cores; i++) {
+    workerThreads.push(new Worker("./worker.js", { workerData: i }));
+  }
+
+  workerThreads.forEach((worker, index) => {
+    worker.on("message", (message) => {
+      resultArray.push({ status: "resolved", data: message });
+      console.log(resultArray);
+    });
+    worker.on("error", (error) => {
+      resultArray.push({ status: "error", data: null });
+    });
+  });
 };
 
 await performCalculations();
